27108d799e8346950f99dd2ad8427e56e46bf381
Karina Pereira <karina.biologa@gmail.com>
2018-07-09 11:36:25 -0300

    Versão 0.0.3 - final (problema com o contador resolvido)

--- Versão 0.0.3/script.js
+++ Versão 0.0.3/script.js
@@ -11,41 +11,51 @@ function postar(){
   clear.value = "";
 }
 
-function alertChar(){
-  var máximo = 140;
-  var conteúdo = document.getElementById("newTweet").value.length;
-  var total = document.getElementById("caracteres").innerHTML;
-  total = máximo -conteúdo;
-  if(total >= 0){
-    if (total <= 20 && total > 10){
-      document.getElementById("caracteres").style.color = '#00FF00';
-    }
-    if (total <= 10 && total >5){
-      document.getElementById("caracteres").style.color = '#FFFF00';
-    }
-    if (total <= 5 && total >= 0){
-      document.getElementById("caracteres").style.color = '#FF8C00';
-    }
-    if (total > 0){
-      document.getElementById("caracteres").style.color = '#FF0000';
-    } else {
-      document.getElementById("button").disabled = true;
-    }
-  }  
-}
+  var máximo = document.getElementById("caracteres").innerHTML;
+  document.getElementById("newTweet").addEventListener("input", function(){
+  var conteúdo = this.value;
+  countChar = conteúdo.split("").length;
+  var total = document.getElementById("caracteres").innerHTML = máximo - countChar;
+  if(countChar >= 0 && countChar <=140){
+    document.getElementById("button").disabled = false;
+  } else {
+    document.getElementById("button").disabled = true;
+  }
+  if(total <= 30 && total > 20){
+    document.getElementById("caracteres").style = "color: #00FF00";
+  }
+  if(total <= 20 && total > 10){
+    document.getElementById("caracteres").style = "color: #FFFF00";
+  }
+  if(total <= 10 && total >= 0){
+    document.getElementById("caracteres").style = "color: #FF8C00";
+  }
+  if(total < 0){
+    document.getElementById("caracteres").style = "color: #FF0000";
+  }
+})
 
-//   var máximo = document.getElementById("caracteres").innerHTML;
-//   document.getElementById("newTweet").addEventListener("input", function(){
-//   var conteúdo = this.value;
-//   countChar = conteúdo.split("").length;
-//   var total = document.getElementById("caracteres").innerHTML = máximo - countChar;
 
-//   if(countChar > 0 && countChar <=140){
-//     document.getElementById("button").disabled = false;
-//   } else {
-//     document.getElementById("button").disabled = true;
-
+// var máximo = document.getElementById("caracteres").innerHTML;
+// document.getElementById("newTweet").addEventListener("input", function(){
+// var conteúdo = this.value;
+// countChar = conteúdo.split("").length;
+// var total = document.getElementById("caracteres").innerHTML = máximo - countChar;
+// if(countChar >= 0){
+//   if(countChar <= 20 && countChar > 10){
+//     document.getElementById("caracteres").style.color = "#00FF00";
 //   }
+//   if(countChar <= 10 && countChar > 5){
+//     document.getElementById("caracteres").style.color = "#FFFF00";
+//   }
+//   if(countChar <= 5 e countChar >= 0){
+//     document.getElementById("caracteres").style.color = "#FF8C00";
+//   }
+//   if(countChar > 0){
+//     document.getElementById("caracteres").style.color = "#FF0000";
+//   }
+//   document.getElementById("button").disabled = false;
+// } else {
+//   document.getElementById("button").disabled = true;
+// }
 // })
-
-
